54b003de86e979ee45a158942e4b8e7c681f9487
Shyana Srikanthalingam <shyanasri@gmail.com>
2019-11-16 21:09:16 -0500

    Update setup.py

--- pythonapp/setup.py
+++ pythonapp/setup.py
@@ -5,6 +5,6 @@ setup(name='Barcode Sustainability Scanner',
       url='https://github.com/shyanasri/FairTradeSustainability/tree/master/python',
       author='Runtime Terror',
       license='MIT',
-      packages=['sustainabilityChecker'],
+      packages=[],
       install_requires=['numpy','imutils','cv2','pyzbar','requests','csv',],
       zip_safe=False)
