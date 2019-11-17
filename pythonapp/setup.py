8a29874fbda6d4d03401be38f086e79d41ea48b9
Shyana Srikanthalingam <shyanasri@gmail.com>
2019-11-16 21:05:05 -0500

    Add files via upload

---
+++ pythonapp/setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+setup(name='Barcode Sustainability Scanner',
+      version='1',
+      description='Determines whether products have Fairtrade certification',
+      url='https://github.com/shyanasri/FairTradeSustainability/tree/master/python',
+      author='Runtime Terror',
+      license='MIT',
+      packages=['sustainabilityChecker'],
+      install_requires=['numpy','imutils','cv2','pyzbar','requests','csv',],
+      zip_safe=False)
